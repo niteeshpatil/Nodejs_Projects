2af8b1c71967151a77d458972bb1f665373a9c52
niteeshpatil <niteeshgpatil5@gmail.com>
2022-10-04 12:45:58 +0530

    get started with weather-app

---
+++ weather-app/app.js
@@ -0,0 +1,20 @@
+// console.log("Starting");
+
+// setTimeout(() => {
+//   console.log("hello 2 sec time");
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("hello 0 sec time");
+// }, 0);
+
+// console.log("End");
+
+const request = require("request");
+const url =
+  "http://api.weatherstack.com/current?access_key=1f5ab46860989bbb0ff8118285f6b919&query=12.9716,77.5946";
+
+request({ url: url }, (error, res) => {
+  const data = JSON.parse(res.body);
+  console.log(data.current);
+});
