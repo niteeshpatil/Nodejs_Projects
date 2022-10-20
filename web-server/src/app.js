f72437a13cb707c82cc9733c0d1bec5c976bb4b9
niteeshpatil <niteeshgpatil5@gmail.com>
2022-10-20 12:31:19 +0530

    creating webserver website to larn express,handabars etc

---
+++ web-server/src/app.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+// console.log(__dirname);
+// console.log(path.join(__dirname,'../public'));
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("views", viewsPath);
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
+// app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "root",
+    name: "niteesh",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "about",
+    name: "niteesh",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "help",
+    name: "niteesh",
+    msg: "we are glade to help",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is runing on port 3000");
+});
