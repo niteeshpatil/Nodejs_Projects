b9c24021694668bb24a03ad93a9b722aa23a792b
niteeshpatil <niteeshgpatil5@gmail.com>
2022-11-09 12:50:02 +0530

    by get request users,user by id,tasks,tasks by id can fatched

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/users");
 const Task = require("./models/task");
+const { get } = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -14,6 +15,56 @@ const port = 3000;
 app.use(express.json());
 // automaticaly parses incoming request to json
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  // console.log(req.params);
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
 app.post("/users", (req, res) => {
   //   console.log(req.body);
   //   res.send("testing!");
@@ -23,7 +74,7 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((error) => {
       res.status(400).send(error);
@@ -36,7 +87,7 @@ app.post("/tasks", (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send(error);
