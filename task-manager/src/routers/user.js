c65ac1be22f6e415200557e6315bebea71f67787
niteeshpatil <niteeshgpatil5@gmail.com>
2022-11-14 16:40:08 +0530

    login,logout logout all setions

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -88,6 +88,29 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedupdates = ["name", "email", "password", "age"];
