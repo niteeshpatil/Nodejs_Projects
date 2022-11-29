97cea9ee858df089fc96ea7f504bfcb396c61e20
niteeshpatil <niteeshgpatil5@gmail.com>
2022-11-29 17:32:22 +0530

    testing the user http request useing supertest npm module

---
+++ task-manager/src/app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("../src/routers/user");
+const taskRouter = require("../src/routers/task");
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+// new middleware
+
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     return res.send("GET Request are disabled");
+//   }
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("website under down");
+// });
+
+app.use(express.json());
+
+// automaticaly parses incoming request to json
+
+app.use(userRouter);
+app.use(taskRouter);
+
+// const jwt = require("jsonwebtoken");
+
+// const myFuntion = async () => {
+//   const token = await jwt.sign({ _id: "1234" }, "thisismypassword");
+//   console.log(token);
+//   const data = await jwt.verify(token, "thisismypassword");
+
+//   console.log(data);
+// };
+
+// myFuntion();
+
+// const multer = require("multer");
+// const uplode = multer({
+//   dest: "images",
+//   limits: {
+//     fileSize: 1000000,
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(doc|docx)$/)) {
+//       return cb(new Error("File must be worddoc"));
+//     }
+
+//     cb(undefined, true);
+//   },
+// });
+
+// app.post("/uplode", uplode.single("uplode"), (req, res) => {
+//   res.send();
+// },(error, req, res, next) => {
+//   res.status(400).send({ error: error.message });
+// });
+
+module.exports = app;
---
+++ task-manager/tests/user.test.js
@@ -0,0 +1,134 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../src/app");
+const jwt = require("jsonwebtoken");
+const User = require("../src/models/users");
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  name: "teesh",
+  email: "teeshgpatil@gmail.com",
+  password: "niteesh",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECREATE),
+    },
+  ],
+};
+
+beforeEach(async () => {
+  //   console.log("start");
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+// afterEach(() => {
+//   console.log("end");
+// });
+
+test("Shouled singup a new user", async () => {
+  const res = await request(app)
+    .post("/users")
+    .send({
+      name: "niteesh",
+      email: "niteeshgpatil@gmail.com",
+      password: "niteesh",
+    })
+    .expect(201);
+
+  const user = await User.findById(res.body.user._id);
+  expect(user).not.toBe(null);
+
+  expect(res.body).toMatchObject({
+    user: {
+      name: "niteesh",
+      email: "niteeshgpatil@gmail.com",
+    },
+    token: user.tokens[0].token,
+  });
+});
+
+test("Should login exsting user", async () => {
+  const res = await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+
+  const user = await User.findById(res.body.user._id);
+
+  expect(user.tokens[0].token).toBe(userOne.tokens[0].token);
+});
+
+test("Should not login not exsting user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "kjdsflk",
+    })
+    .expect(400);
+});
+
+test("should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("should not get profile for Unauthorized user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("should delete profile for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+
+  expect(user).toBe(null);
+});
+
+test("should not delete profile for Unauthorized user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
+});
+
+test("should uplode profile pic", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ name: "sateesh" })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+
+  expect(user.name).toEqual("sateesh");
+});
+
+test("Should update user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ location: "bangalure" })
+    .expect(400);
+});
