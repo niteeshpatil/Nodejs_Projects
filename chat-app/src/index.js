fcbe0ca43265d7daedae54014e4cfa7b84ae5e1e
niteeshpatil <niteeshgpatil5@gmail.com>
2022-11-30 18:28:47 +0530

    exploring the websockests(socket.io) for client sever communication

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const port = 3000;
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("chat-app");
+const port = process.env.PORT || 3000;
+const publicDirPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirPath));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
-app.listen(port, () => {
-  console.log("app listening on port: " + port);
+server.listen(port, () => {
+  console.log("sever listening on port: " + port);
 });
